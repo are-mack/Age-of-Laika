60c8c29d6b006be483925e0711dc5ac34890421f
McCawley <McCawley@ryMacBP.local>
2014-06-16 14:10:56 -0400

    Initial Commit

---
+++ ageOfLaikaTests/ageOfLaikaTests.m
@@ -0,0 +1,34 @@
+//
+//  ageOfLaikaTests.m
+//  ageOfLaikaTests
+//
+//  Created by McCawley on 6/16/14.
+//  Copyright (c) 2014 Ryan McCawley. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ageOfLaikaTests : XCTestCase
+
+@end
+
+@implementation ageOfLaikaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
